7ef1bea7390747264b8ee76419f8fb67c599e470
kmcgee3 <31813551+kmcgee3@users.noreply.github.com>
2017-11-09 13:30:54 -0600

    Add files via upload

---
+++ js/jscript.js
@@ -0,0 +1,23 @@
+//Code to execute when document is loaded
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+// OR
+
+//Code to execute when window is loaded
+$( window ).on( "load", function() {
+        console.log( "window loaded" );
+    });
+
+
+//JavaScript Variables
+var person = "John Doe";
+
+//Insert Function Here
+function functionName(parameters) {
+  code to be executed
+}
+
+//Insert Methods
+methodName : function() { code lines }
